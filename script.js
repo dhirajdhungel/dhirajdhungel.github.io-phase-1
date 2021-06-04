99a54849f327ac6133faeeee236c67b6b47230d6
dhirajdhungel <dhirajdhungel@github.com>
2021-06-05 02:08:48 +0545

    Initial Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const navSlide = function(){
+    var burger = document.querySelector(".burger"),
+        nav = document.querySelector('.page-nav'),
+        navLinks = document.querySelectorAll('nav.page-nav a'); /* target the links inside the nav instead of the nav itself (fixes closing animation) */
+
+        burger.addEventListener("click", function(){
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link,index) => {
+            if (link.style.animation){
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.2s ease forwards ${index / 3 + 0}s`; /* index / 3 as there are 3 links inside the nav */
+            }
+        });
+        
+        burger.classList.toggle('toggle'); 
+    });
+}
+navSlide();
