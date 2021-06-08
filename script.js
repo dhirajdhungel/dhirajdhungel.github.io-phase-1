4163228a702d608efdbf4fbab5db38a73a060ef6
dhirajdhungel <dhirajdhungel@github.com>
2021-06-08 09:27:16 +0545

    index.html,style.scss: learning section left to fix

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-const navSlide = function(){
-    var burger = document.querySelector(".burger"),
-        nav = document.querySelector('.page-nav'),
-        navLinks = document.querySelectorAll('nav.page-nav a'); /* target the links inside the nav instead of the nav itself (fixes closing animation) */
+// const navSlide = function(){
+//     var burger = document.querySelector(".burger"),
+//         nav = document.querySelector('.page-nav'),
+//         navLinks = document.querySelectorAll('nav.page-nav a'); /* target the links inside the nav instead of the nav itself (fixes closing animation) */
 
-        burger.addEventListener("click", function(){
-        nav.classList.toggle('nav-active');
+//         burger.addEventListener("click", function(){
+//         nav.classList.toggle('nav-active');
 
-        navLinks.forEach((link,index) => {
-            if (link.style.animation){
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.2s ease forwards ${index / 3 + 0}s`; /* index / 3 as there are 3 links inside the nav */
-            }
-        });
+//         navLinks.forEach((link,index) => {
+//             if (link.style.animation){
+//                 link.style.animation = '';
+//             } else {
+//                 link.style.animation = `navLinkFade 0.2s ease forwards ${index / 3 + 0}s`; /* index / 3 as there are 3 links inside the nav */
+//             }
+//         });
         
-        burger.classList.toggle('toggle'); 
-    });
-}
-navSlide();
+//         burger.classList.toggle('toggle'); 
+//     });
+// }
+// navSlide();
+
+function myFunction(x) {
+    x.classList.toggle("toggle");
+  }
